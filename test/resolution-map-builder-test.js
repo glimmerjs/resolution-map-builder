c8724ebab02dba24606f1710630d3179aaecc5c3
Robert Jackson <rjackson@linkedin.com>
2017-04-06 21:26:44 -0400

    Refactor tests to use broccoli-test-helper.

--- test/resolution-map-builder-test.js
+++ test/resolution-map-builder-test.js
@@ -1,40 +1,88 @@
 'use strict';
 
 const ResolutionMapBuilder = require('..');
-const build = require('broccoli-fixture').build;
 const path = require('path');
+const fs = require('fs');
+const co = require('co');
 const assert = require('assert');
+const BroccoliTestHelper = require('broccoli-test-helper');
+const buildOutput = BroccoliTestHelper.buildOutput;
+const createTempDir = BroccoliTestHelper.createTempDir;
 
 describe('resolution-map-builder', function() {
-  it('can read a config file, parse modules, and log specifiers (if requested)', function() {
-    let src = path.join(process.cwd(), 'test', 'fixtures', 'src');
-    let config = path.join(process.cwd(), 'test', 'fixtures', 'config');
-    let options = { configPath: 'environment.json', logSpecifiers: true };
+  let configFixture, srcFixture;
 
-    let mapBuilder = new ResolutionMapBuilder(src, config, options);
+  beforeEach(function() {
+    return Promise.all([
+      createTempDir(),
+      createTempDir()
+    ]).then((results) => {
+      srcFixture = results[0];
+      configFixture = results[1];
 
-    return build(mapBuilder)
-      .then(result => {
-        assert.deepEqual(
-          mapBuilder.specifiers.sort(),
-          [
-            'component:/my-app/components/my-app',
-            'template:/my-app/components/my-app',
-
-            'component:/my-app/components/text-editor',
-            'template:/my-app/components/text-editor',
-
-            'component:/my-app/components/my-app/page-banner',
-            'template:/my-app/components/my-app/page-banner',
-            'component:/my-app/components/my-app/page-banner/titleize'
-          ].sort()
-        );
+      srcFixture.write({
+        "src": {
+          "ui": {
+            "components": {
+              "my-app": {
+                "README.md": "## My-App Component\n",
+                "component.ts": "",
+                "page-banner": {
+                  "-utils": {
+                    "ignore-me.ts": ""
+                  },
+                  "component.ts": "",
+                  "template.hbs": "",
+                  "titleize.ts": ""
+                },
+                "template.hbs": ""
+              },
+              "text-editor.hbs": "",
+              "text-editor.ts": ""
+            },
+            "index.html": "<html>\n    <head></head>\n    <body></body>\n</html>\n"
+          }
+        }
       });
+
+      let configPath = path.join(__dirname, 'fixtures', 'config', 'environment.json');
+      configFixture.write({
+        'environment.json': fs.readFileSync(configPath, { encoding: 'utf8' })
+      })
+    });
   });
 
-  it('can use config options if no config file exists', function() {
-    let src = path.join(process.cwd(), 'test', 'fixtures', 'src');
-    let config = path.join(process.cwd(), 'test', 'fixtures', 'config');
+  afterEach(function() {
+    return Promise.all([
+      srcFixture.dispose(),
+      configFixture.dispose()
+    ]);
+  });
+
+  it('can read a config file, parse modules, and log specifiers (if requested)', co.wrap(function* () {
+    let options = { configPath: 'environment.json', logSpecifiers: true };
+
+    let mapBuilder = new ResolutionMapBuilder(srcFixture.path() + '/src', configFixture.path(), options);
+
+    yield buildOutput(mapBuilder);
+
+    assert.deepEqual(
+      mapBuilder.specifiers.sort(),
+      [
+        'component:/my-app/components/my-app',
+        'template:/my-app/components/my-app',
+
+        'component:/my-app/components/text-editor',
+        'template:/my-app/components/text-editor',
+
+        'component:/my-app/components/my-app/page-banner',
+        'template:/my-app/components/my-app/page-banner',
+        'component:/my-app/components/my-app/page-banner/titleize'
+      ].sort()
+    );
+  }));
+
+  it('can use config options if no config file exists', co.wrap(function* () {
     let options = {
       defaultModulePrefix: 'my-app',
       defaultModuleConfiguration: {
@@ -64,24 +112,23 @@ describe('resolution-map-builder', function() {
       logSpecifiers: true
     };
 
-    let mapBuilder = new ResolutionMapBuilder(src, config, options);
+    let mapBuilder = new ResolutionMapBuilder(srcFixture.path() + '/src', configFixture.path(), options);
 
-    return build(mapBuilder)
-      .then(result => {
-        assert.deepEqual(
-          mapBuilder.specifiers.sort(),
-          [
-            'component:/my-app/components/my-app',
-            'template:/my-app/components/my-app',
+    yield buildOutput(mapBuilder);
 
-            'component:/my-app/components/text-editor',
-            'template:/my-app/components/text-editor',
+    assert.deepEqual(
+      mapBuilder.specifiers.sort(),
+      [
+        'component:/my-app/components/my-app',
+        'template:/my-app/components/my-app',
 
-            'component:/my-app/components/my-app/page-banner',
-            'template:/my-app/components/my-app/page-banner',
-            'component:/my-app/components/my-app/page-banner/titleize'
-          ].sort()
-        );
-      });
-  });
+        'component:/my-app/components/text-editor',
+        'template:/my-app/components/text-editor',
+
+        'component:/my-app/components/my-app/page-banner',
+        'template:/my-app/components/my-app/page-banner',
+        'component:/my-app/components/my-app/page-banner/titleize'
+      ].sort()
+    );
+  }));
 });
